0bb25c94897c64e86b4a07463103029cea6011c0
yangshiwei <swearer23@163.com>
2022-01-16 21:23:12 +0800

    支持input类型

---
+++ components/input/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+    <h2>文本域</h2>
+    <el-row
+      type="flex"
+      span='6'
+      style="margin-bottom: 20px;"
+    >
+      <el-input v-model="input" placeholder="请输入内容" style="width: 300px"></el-input>
+    </el-row>
+    <Configuration section="input" />
+  </section>
+</template>
+<script>
+import Configuration from '../configuration'
+
+export default {
+  components: {Configuration},
+  data: () => {
+    return {
+      input: ''
+    }
+  }
+}
+</script>
+<style></style>
